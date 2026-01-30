528120cb7c7d648473964e6aae339e042fa16ddb
Landers9 <franckaigba4@gmail.com>
2026-01-30 15:08:11 +0100

    Amélioration des métadonnées PyPI + lien GitHub

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='portier',
-    version='0.1.0',
+    name='portier-cli',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'click>=8.0.0',
@@ -15,7 +18,38 @@ setup(
             'portier=portier.cli:main',
         ],
     },
-    author='Ton Nom',
-    description='Gestionnaire de ports pour applications Docker',
+
+    # Informations affichées sur PyPI
+    author='Franck AÏGBA',
+    author_email='franckaigba4@gmail.com',
+    description='Gestionnaire de ports pour vos applications Docker',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    # Lien vers GitHub
+    url='https://github.com/Landers9/portier',
+    project_urls={
+        'Bug Tracker': 'https://github.com/Landers9/portier/issues',
+        'Documentation': 'https://github.com/Landers9/portier#readme',
+        'Source': 'https://github.com/Landers9/portier',
+    },
+
+    # Métadonnées
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    keywords='docker, ports, cli, devops, containers',
     python_requires='>=3.8',
 )
